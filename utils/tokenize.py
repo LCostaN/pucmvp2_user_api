826fabec7703a03f33b38c94001f3ae958a35368
LCostaN <33222832+LCostaN@users.noreply.github.com>
2023-09-22 22:51:53 -0300

    remove secret2

--- utils/tokenize.py
+++ utils/tokenize.py
@@ -2,7 +2,6 @@ import os
 import jwt
 
 secret = os.environ['SECRET']
-secret2 = os.environ['SECRET2']
 
 def tokenize(username: str):
   return jwt.encode({"username": username}, secret, algorithm="HS256")
